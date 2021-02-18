5a30218cfd136318af6e1bdd7650d935234a48f1
hoannv <hoannv@sphinxjsc.com>
2021-02-18 23:02:40 +0700

    bai 10

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path')
+const morgan = require('morgan');
+const handlebars  = require('express-handlebars');
+const app = express();
+const port = 3000;
+app.use(morgan('combined'));
+app.engine('hbs', handlebars({
+  extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views',path.join(__dirname, 'resource/views'));
+console.log("path.join(__dirname, 'resourceviews') ",path.join(__dirname, 'resource/views'));
+app.get('/', (req, res) => {
+  res.render('home');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+})
