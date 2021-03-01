dea889ab7300c10a414caf3ae30cc7e0defe2690
hoannv <hoannv@sphinxjsc.com>
2021-03-01 18:01:18 +0700

    update code

--- src/routes/news.js
+++ src/routes/news.js
@@ -1,7 +1,7 @@
 const express = require('express');
 var app = express();
-  const router = express.Router();
+const router = express.Router();
 
-    const newsController = require('./../app/controller/NewsControllers');
-   router.use('/', newsController.index);
+const newsController = require('./../app/controller/NewsControllers');
+router.use('/', newsController.index);
 module.exports = router;
